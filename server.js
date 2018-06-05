413bf17bf6949e1a80e53c01cd38068f801a270f
pipplogic <pipplogic@gmail.com>
2018-06-04 21:53:46 -0500

    starting server side work

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require("path");
 const express = require("express");
 const compression = require("compression");
+const request = require("request");
+const API_HOST = process.env.API_HOST || "https://gci-pickem.herokuapp.com";
 
 const port = process.env.PORT || 3002;
 const app = express();
@@ -8,6 +10,18 @@ const app = express();
 app.use(compression());
 
 app.use(express.static(path.join(__dirname, "/build")));
+
+app.get("/api/years/:year/weeks/:week/games", (req, res) => {
+  const { params: { year, week } } = req;
+
+  request.get(`${API_HOST}/api/v1/games/season/${year}/week/${week}`, function(
+    err,
+    msg,
+    body
+  ) {
+    res.json(JSON.parse(body));
+  });
+});
 
 app.get(["/api", "/api/*"], (req, res) => {
   res.status(404).send("Not implemented");
@@ -18,3 +32,4 @@ app.get("*", (req, res) => {
 });
 
 app.listen(port);
+console.log(`Running on port ${port}`);
