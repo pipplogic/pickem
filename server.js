adbd3f5fe0da00ac07dc306bbca68be9b601099e
pipplogic <pipplogic@gmail.com>
2018-08-01 22:29:23 -0500

    Redirect http requests to https in production

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const path = require("path");
 const express = require("express");
 const compression = require("compression");
 const request = require("request");
+const redirectToHTTPS = require("express-http-to-https").redirectToHTTPS;
 
 if (process.env.NODE_ENV !== "production") {
   require("dotenv").load();
@@ -28,14 +29,9 @@ app.use(express.static(path.join(__dirname, "/build")));
 app.use(express.json());
 app.use(compression());
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
-});
+if (process.env.NODE_ENV === "production") {
+  app.use(redirectToHTTPS());
+}
 
 app.post("/login", (req, res) => {
   const { body: { username, password } } = req;
